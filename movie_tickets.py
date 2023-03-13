1461b1f36a9054c03a6d2b906e02964807dfb4e3
Wudthedud <wud@middleton.school.nz>
2023-03-14 10:15:50 +1300

    Create movie_tickets.py

---
+++ movie_tickets.py
@@ -0,0 +1,16 @@
+"""Movie Theatre Ticketing System - Version 1
+Welcome screen and variables
+Create by Daniel Wu"""
+
+# Component 1 - Welcome screen and set up variables
+def sell_ticket():
+    print("********** Fanfare Movies - Ticketing System **********\n")
+    adult_tickets = 0
+    child_tickets = 0
+    student_tickets = 0
+    gift_vouchers = 0
+    tickets_sold = 0
+    total_sales = 0
+
+# Main Routine
+sell_ticket()
