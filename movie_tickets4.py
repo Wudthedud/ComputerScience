818e8ca6f8f5b21c9a405fa79446736383821b51
Wudthedud <wud@middleton.school.nz>
2023-03-15 11:36:39 +1300

    Update movie_tickets4.py

--- movie_tickets4.py
+++ movie_tickets4.py
@@ -2,12 +2,29 @@
 Calculate Ticket Price
 Create by Daniel Wu"""
 
+
 # Component 3
 def get_price(type_):
     prices = [["A", 12.5], ["C", 7], ["S", 9], ["G", 0]]
     for price in prices:
-        if price [0] == type_:
+        if price[0] == type_:
             return price[1]
+
+
+# Component 4
+def confirm_order(ticket, number, cost):
+    confirm = ""
+    while confirm != "Y" and confirm != "N":
+        confirm = input(f"\nYou have ordered {num_tickets} {ticket_type}(s) at a cost of ${cost * num_tickets:.2f}!\n"
+                        f"Confirm? (Y/N) \n>>").upper()
+        if confirm == "Y":
+            return True
+        elif confirm == "N":
+            return False
+        else:
+            print("Please choose (Y/N)")
+
+
 
 
 # Component 1 - Welcome screen and set up variables
@@ -20,10 +37,10 @@ def sell_ticket():
     tickets_sold = 0
     total_sales = 0
 
+
 # Component 2
-
 ticket_wanted = "Y"
-
+total = float(0)
 while ticket_wanted == "Y":
     ticket_type = input("What kind of ticket do you want: \n"
                         "\t 'A' - Adult Ticket \n"
@@ -37,12 +54,14 @@ while ticket_wanted == "Y":
     else:
         num_tickets = int(input(f"How many {ticket_type} tickets do you want? \n "))
         cost = get_price(ticket_type)
-        print(f"\nYou have ordered {num_tickets} {ticket_type}(s) at a cost of ${cost * num_tickets:.2f}!")
+        if confirm_order(ticket_type, num_tickets, cost):
+            print("Order Confirmed")
+        else:
+            print("Order Cancelled")
+
+        total = total + cost
+
     ticket_wanted = input("Do you want to sell another ticket? (Y/N) \n").upper()
-
-
 
 # Main Routine
 sell_ticket()
-
-
